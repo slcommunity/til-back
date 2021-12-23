16419fba063c11e08852f46c99a3f1a54db6980c
chanhong-dev <zxck03051@gmail.com>
2021-12-24 02:01:55 +0900

    Comment domain test - [#3]

--- src/main/java/com/example/tilproject/utils/CommentValidator.java
+++ src/main/java/com/example/tilproject/utils/CommentValidator.java
@@ -1,4 +1,17 @@
 package com.example.tilproject.utils;
 
+import com.example.tilproject.domain.User;
+import com.example.tilproject.exception.ApiRequestException;
+
 public class CommentValidator {
+
+    public static void validateCommentCreate(String content, User user){
+        if(user == null) {
+            throw new ApiRequestException("유저 정보가 유효하지 않습니다.");
+        }
+        if(content == null || content.length() == 0) {
+            throw new ApiRequestException("댓글 내용은 필수 입력사항 입니다.");
+        }
+    }
+
 }
